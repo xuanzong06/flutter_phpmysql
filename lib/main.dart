8e182fdf71e00230d65dd1eac5aaebc8a9e5c388
yge <xuanzong06@gmail.com>
2022-04-05 23:52:59 +0800

    success

--- lib/main.dart
+++ lib/main.dart
@@ -1,116 +1,264 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-
+import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(const MyApp());
+  runApp(HomeRoute());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class HomeRoute extends StatefulWidget {
+  const HomeRoute({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  _HomeRouteState createState() => _HomeRouteState();
+}
+
+class _HomeRouteState extends State<HomeRoute> {
+  TextEditingController? _firstNameController;
+  TextEditingController? _lastNameController;
+
+  @override
+  void initState() {
+    super.initState();
+    _firstNameController = TextEditingController();
+    _lastNameController = TextEditingController();
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  // Now lets add an Employee
+  _addEmployee() {
+    if (_firstNameController!.text.isEmpty || _lastNameController!.text.isEmpty) {
+      print('Empty Fields');
+      return;
+    }
+    // _showProgress('Adding Employee...');
+    print('_firstNameController!.text : '+_firstNameController!.text+ '   _lastNameController!.text :   '+_lastNameController!.text);
+    Services.addEmployee(_firstNameController!.text, _lastNameController!.text)
+        .then((result) {
+          print(result);
+      if ('success' == result) {
+        print('successed');
+        // _getEmployees(); // Refresh the List after adding each employee...
+        // _clearValues();
+        print("Add Employee Work Successed!!");
+      }
     });
   }
 
+  // 先從最基本的開始，是否可以傳值給php
+  _addEmployee2() {
+    addEmployee2(_firstNameController!.text, _lastNameController!.text).then((result) {
+      print(result);
+      if ('success' == result) {
+        print('successed');
+        // _getEmployees(); // Refresh the List after adding each employee...
+        // _clearValues();
+        print("Add Employee Work Successed!!");
+      }
+    });
+  }
+
+  static Future<String> addEmployee2(String firstName, String lastName) async {
+    try {
+      var map = Map<String, dynamic>();
+      map['action'] = "flutter";
+      // map['first_name'] = firstName;
+      // map['last_name'] = lastName;
+      // final response = await http.post(Uri. parse('http://localhost:8888/testdb.php'), body: map);
+      final response = await http.post(Uri. parse('http://192.168.31.167:8888/testdb.php'));
+      print('addEmployee Response: ${response.body}');
+      if (200 == response.statusCode) {
+        return response.body;
+      } else {
+        return "error";
+      }
+    } catch (e) {
+      return "error";
+    }
+  }
+  //function that I create for button event
+  // _InsertProgress(){
+  //   if(_firstNameController.text.isEmpty || _lastNameController.text.isEmpty){
+  //
+  //   }
+  // }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Title"),
+        ),
+        body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                child: TextField(
+                  controller: _firstNameController,
+                  decoration: InputDecoration.collapsed(
+                    hintText: 'First Name',
+                  ),
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                child: TextField(
+                  controller: _lastNameController,
+                  decoration: InputDecoration.collapsed(
+                    hintText: 'Last Name',
+                  ),
+                ),
+              ),
+            ),
+            Row(
+              children: <Widget>[
+                OutlinedButton(
+                  onPressed: () {
+                    // if (_firstNameController!.text.isEmpty ||
+                    //     _lastNameController!.text.isEmpty) {
+                    //   //_..Controller 加上驚嘆號的用途是檢查該參數是否為null，也可用問號，但問號不可以在條件中使用
+                    //   print('欄位沒有值');
+                    //   return; //跳出工作
+                    // }
+                    _addEmployee2();
+                    print('Click Add Button');
+                  },
+                  child: Text('Add'),
+                )
+              ],
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class Employee {
+  int? id;
+  String? firstName;
+  String? lastName;
+
+  Employee({this.id, this.firstName, this.lastName});
+
+  factory Employee.fromJson(Map<String, dynamic> json) {
+    return Employee(
+      id: json['id'] as int,
+      firstName: json['first_name'] as String,
+      lastName: json['last_name'] as String,
+    );
+  }
+}
+
+class Services {
+  static const ROOT = 'http://localhost:8080/employee_actions.php';
+  static const _CREATE_TABLE_ACTION = 'CREATE_TABLE';
+  static const _GET_ALL_ACTION = 'GET_ALL';
+  static const _ADD_EMP_ACTION = 'ADD_EMP';
+  static const _UPDATE_EMP_ACTION = 'UPDATE_EMP';
+  static const _DELETE_EMP_ACTION = 'DELETE_EMP';
+
+  // Method to create the table Employees.
+  static Future<String> createTable() async {
+    try {
+      // add the parameters to pass to the request.
+      var map = Map<String, dynamic>();
+      map['action'] = _CREATE_TABLE_ACTION;
+      final response = await http.post(Uri.parse(ROOT), body: map);
+      print('Create Table Response: ${response.body}');
+      if (200 == response.statusCode) {
+        return response.body;
+      } else {
+        return "error";
+      }
+    } catch (e) {
+      return "error";
+    }
+  }
+
+  static Future<List<Employee>> getEmployees() async {
+    try {
+      var map = Map<String, dynamic>();
+      map['action'] = _GET_ALL_ACTION;
+      final response = await http.post(Uri.parse(ROOT), body: map);
+      print('getEmployees Response: ${response.body}');
+      if (200 == response.statusCode) {
+        List<Employee> list = parseResponse(response.body);
+        return list;
+      } else {
+        List<Employee> result = <Employee>[];
+        return result;
+      }
+    } catch (e) {
+      List<Employee> result = <Employee>[];
+      return result; // return an empty list on exception/error
+    }
+  }
+
+  static List<Employee> parseResponse(String responseBody) {
+    final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
+    return parsed.map<Employee>((json) => Employee.fromJson(json)).toList();
+  }
+
+  // Method to add employee to the database...
+  static Future<String> addEmployee(String firstName, String lastName) async {
+    try {
+      var map = Map<String, dynamic>();
+      map['action'] = _ADD_EMP_ACTION;
+      map['first_name'] = firstName;
+      map['last_name'] = lastName;
+      final response = await http.post(Uri. parse(ROOT), body: map);
+      print('addEmployee Response: ${response.body}');
+      if (200 == response.statusCode) {
+        return response.body;
+      } else {
+        return "error";
+      }
+    } catch (e) {
+      return "error";
+    }
+  }
+
+  // Method to update an Employee in Database...
+  static Future<String> updateEmployee(
+      String empId, String firstName, String lastName) async {
+    try {
+      var map = Map<String, dynamic>();
+      map['action'] = _UPDATE_EMP_ACTION;
+      map['emp_id'] = empId;
+      map['first_name'] = firstName;
+      map['last_name'] = lastName;
+      final response = await http.post(Uri.parse(ROOT), body: map);
+      print('updateEmployee Response: ${response.body}');
+      if (200 == response.statusCode) {
+        return response.body;
+      } else {
+        return "error";
+      }
+    } catch (e) {
+      return "error";
+    }
+  }
+
+  // Method to Delete an Employee from Database...
+  static Future<String> deleteEmployee(String empId) async {
+    try {
+      var map = Map<String, dynamic>();
+      map['action'] = _DELETE_EMP_ACTION;
+      map['emp_id'] = empId;
+      final response = await http.post(Uri.parse(ROOT), body: map);
+      print('deleteEmployee Response: ${response.body}');
+      if (200 == response.statusCode) {
+        return response.body;
+      } else {
+        return "error";
+      }
+    } catch (e) {
+      return "error"; // returning just an "error" string to keep this simple...
+    }
   }
 }
